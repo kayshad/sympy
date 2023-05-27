37a58daa5a5c9907e104018a60e30673aef70412
kayshad <kaisslama@gmail.com>
2023-05-27 13:11:51 +0200

    Update expnotation.py

--- expnotation.py
+++ expnotation.py
@@ -3,26 +3,29 @@ from IPython.display import display, Latex
 from sympy import *
 init_printing(use_latex=True)
 from sympy.printing.str import StrPrinter
-nbr = 1000.0023500814000
-n = Float(str(nbr))
-s = f"{S(StrPrinter({'full_prec': True}).doprint(n)):e}"
-r = Rational(s)
-if nbr == 0 :
-    lat = '$0$'
-elif nbr != 0 and isinstance(nbr,int) and nbr < 10:
-    lat = '$'+s[0]+'.10^{'+ s.split('e')[1]+'}'+'$'
-else :
-    if nbr >0 and nbr < 1 :
-        if s.split('e')[0].split('.')[1].rstrip('0') != '':
-            lat = '$'+s[0]+','+s.split('e')[0].split('.')[1].rstrip('0')+'.10^{'+ s.split('e')[1]+'}'+'$'
-        else :
-            lat = '$'+s[0]+s.split('e')[0].split('.')[1].rstrip('0')+'.10^{'+ s.split('e')[1]+'}'+'$'
+def nbrexpos(nbr):
+    nbr = nbr
+    n = Float(str(nbr))
+    s = f"{S(StrPrinter({'full_prec': True}).doprint(n)):e}"
+    r = Rational(s)
+    if nbr == 0 :
+        lat = '$0$'
+    elif nbr != 0 and isinstance(nbr,int) and nbr < 10:
+        lat = '$'+s[0]+'.10^{'+ s.split('e')[1]+'}'+'$'
     else :
-        if s.split('e')[0].split('.')[1].rstrip('0') != '':
-            lat = '$'+s[0]+ ',' +s.split('e')[0].split('.')[1].rstrip('0')+'.10^{'+ s.split('e')[1]+'}'+'$'
+        if nbr >0 and nbr < 1 :
+            if s.split('e')[0].split('.')[1].rstrip('0') != '':
+                lat = '$'+s[0]+','+s.split('e')[0].split('.')[1].rstrip('0')+'.10^{'+ s.split('e')[1]+'}'+'$'
+            else :
+                lat = '$'+s[0]+s.split('e')[0].split('.')[1].rstrip('0')+'.10^{'+ s.split('e')[1]+'}'+'$'
         else :
-            lat = '$'+s[0] +s.split('e')[0].split('.')[1].rstrip('0')+'.10^{'+ s.split('e')[1]+'}'+'$'
+            if s.split('e')[0].split('.')[1].rstrip('0') != '':
+                lat = '$'+s[0]+ ',' +s.split('e')[0].split('.')[1].rstrip('0')+'.10^{'+ s.split('e')[1]+'}'+'$'
+            else :
+                lat = '$'+s[0] +s.split('e')[0].split('.')[1].rstrip('0')+'.10^{'+ s.split('e')[1]+'}'+'$'
+    return lat
             
 
 #display(nbr,n,s,r,Latex(lat))
-display(n,r.evalf(),Latex(lat))
+#display(n,r.evalf(),Latex(lat))
+display(Latex(nbrexpos(1)))
